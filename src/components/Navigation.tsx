ecd10c92670e4b0b6e612519c770233e6964dee5
Joselyne uwingabire <uwingajoselyne@gmail.com>
2024-01-20 20:32:56 +0200

    adding link to the navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,8 +1,18 @@
+import { Link } from 'react-router-dom'
+import MenuIcon from '../assets/MenuIcon'
+import Logo from '../assets/Logo'
+import ProfileIcon from '../assets/ProfileIcon'
 const Navigation = () => (
-  <div className="bg-indigo-tertiary">
-    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non magni voluptates adipisci
-    distinctio consequatur culpa exercitationem id quo quas? Distinctio culpa natus qui veniam at
-    saepe magni vitae quo. Officiis?
+  <div className=" flex items-center justify-between rounded-b-lg border border-indigo-tertiary bg-indigo-tertiary px-5 text-white">
+    <Link to="/menu" className="h-6 w-6">
+      <MenuIcon />
+    </Link>
+    <Link to="/user">
+      <Logo className="relative top-3 w-16" />
+    </Link>
+    <Link to="/landingpage">
+      <ProfileIcon className="h-6 w-6" />
+    </Link>
   </div>
 )
 
