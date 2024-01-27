1923380c0f2f6af3c6f884ef64558abd91a77f16
Joselyne uwingabire <uwingajoselyne@gmail.com>
2024-01-27 18:13:24 +0200

    making interface for inputfield

--- src/util/interface.ts
+++ src/util/interface.ts
@@ -1,41 +1,11 @@
+import { ButtonHTMLAttributes, InputHTMLAttributes, PropsWithChildren } from 'react'
 import { UseFormRegisterReturn } from 'react-hook-form'
-import { CarDto, CarTypeDto } from './api'
-import {
-  ButtonHTMLAttributes,
-  PropsWithChildren,
-  SelectHTMLAttributes,
-  InputHTMLAttributes,
-  ReactNode,
-} from 'react'
-
-export interface CarCardProps extends CarDto {
-  carType?: CarTypeDto
-  owner?: { name?: string }
-  onRefetchCars?: () => void
-  showDeleteButton: boolean
-}
 
 export interface ButtonProps extends PropsWithChildren<ButtonHTMLAttributes<HTMLButtonElement>> {
   color?: 'filled' | 'outlined' | 'danger'
 }
 
-export interface MenuItem {
-  id: number
-  name: string
-  href?: string
-  icon: React.ReactNode
-}
-
-export interface SelectInputProps
-  extends PropsWithChildren<SelectHTMLAttributes<HTMLSelectElement>> {
-  name: 'type' | 'fuelType'
-  label: string
-  items?: { id: number; value: string }[]
-  className?: string
-  register: UseFormRegisterReturn
-}
-
-export interface TextInputProps extends PropsWithChildren<InputHTMLAttributes<HTMLInputElement>> {
+export interface InputProps extends PropsWithChildren<InputHTMLAttributes<HTMLInputElement>> {
   name:
     | 'name'
     | 'type'
@@ -49,20 +19,4 @@ export interface TextInputProps extends PropsWithChildren<InputHTMLAttributes<HT
   type?: string
   className?: string
   register: UseFormRegisterReturn
-}
-
-export interface PopUpButtonProps {
-  handleCancelClick: () => void
-}
-
-export interface DeleteModalProps {
-  isOpen: boolean
-  onTriggerModal: () => void
-  onSubmit: () => void
-  children: ReactNode
-}
-
-export interface DeleteButtonProps {
-  onClick: () => void
-  children: ReactNode
 }
