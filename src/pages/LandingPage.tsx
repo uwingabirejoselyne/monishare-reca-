c4d0c11f54bf67038fae63bed65ec869ba80c9e8
Joselyne uwingabire <uwingajoselyne@gmail.com>
2024-01-27 09:50:20 +0200

    zod installation

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,19 +1,23 @@
+import { useNavigate } from 'react-router-dom'
 import Button from '../components/UI/Button'
 
-const LandingPage = () => (
-  <div className="flex flex-col gap-10 px-4">
-    <p className="flex flex-col items-center pt-24 font-lora text-5xl text-white">
-      <span className="font-bold uppercase">moni</span>
-      <span className="font-medium italic">share</span>
-    </p>
-    <p className="flex flex-col items-center truncate py-10 font-lora text-xl font-thin text-white sm:px-14 sm:text-2xl">
-      <span>Start Sharing Your Monis</span>
-      <span>wiht the world</span>
-    </p>
-    <Button color="filled" className="mx-auto w-full max-w-md">
-      Log in
-    </Button>
-  </div>
-)
+const LandingPage = () => {
+  const navigate = useNavigate()
+  return (
+    <div className="flex flex-col gap-10 px-4">
+      <p className="flex flex-col items-center pt-24 font-lora text-5xl text-white">
+        <span className="font-bold uppercase">moni</span>
+        <span className="font-medium italic">share</span>
+      </p>
+      <p className="flex flex-col items-center truncate py-10 font-lora text-xl font-thin text-white sm:px-14 sm:text-2xl">
+        <span>Start Sharing Your Monis</span>
+        <span>wiht the world</span>
+      </p>
+      <Button color="filled" className="mx-auto w-full max-w-md" onClick={() => navigate('/login')}>
+        Log in
+      </Button>
+    </div>
+  )
+}
 
 export default LandingPage
