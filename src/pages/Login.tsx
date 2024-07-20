1ae639536bb68e76f3a51eeeb6761007e2756ea2
uwingabirejoselyne <uwingajoselyne@gmail.com>
2024-07-20 15:55:30 +0200

    react-hook-form installation

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,3 @@
-import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { LoginValues } from '../util/type'
 import { loginValidationSchema } from '../util/ValidationSchema'
